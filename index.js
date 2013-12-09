39ec8e346fec652a83eb40b8f58c7745fb416277
arnorhs <arnorhs@gmail.com>
2013-12-09 04:22:34 +0000

    have breadcrumbs now

--- index.js
+++ index.js
@@ -3,11 +3,14 @@ var path = require('path');
 var respond = require('./lib/respond');
 var fs = require('fs');
 var handlers = require('./lib/response-handlers');
+var querystring = require('querystring');
 
 module.exports = function(opts) {
     // XXX Add xtend or something to do this
     opts = opts || {};
     opts.rootDir = opts.rootDir || process.cwd();
+    opts.inlineExt = opts.inlineExt || ['md', 'markdown', 'txt'];
+    opts.title = opts.title || "weaki";
     server(requestHandler(opts));
 };
 
@@ -16,21 +19,39 @@ if (!module.parent) {
     module.exports();
 }
 
+function generateBreadcrumbs(opts, file) {
+    var relative = path.relative(opts.rootDir, file);
+    var root = { title: "home", url: "/" };
+    if (relative === "") {
+        return [root];
+    }
+    var paths = relative.split("/");
+    var last = "";
+    var breadcrumbs = paths.map(function(section) {
+        return {
+            title: section || "/",
+            url: (last = last + "/" + section)
+        };
+    });
+    breadcrumbs.unshift(root);
+    return breadcrumbs;
+}
+
 function urler(url) {
     var parts = url.split('?');
-    var url = parts[0].replace(/\.\./g, '').substr(1);
     return {
         qs: parts[1] ? querystring.parse(parts[1]) : '',
-        url: url
+        url: parts[0].replace(/\.\./g, '').substr(1)
     };
 }
 
 function requestHandler(opts) {
-    var h = handlers(opts.rootDir);
+    var h = handlers(opts);
 
     return function(req, res) {
         var url = urler(req.url);
         var file = path.resolve(opts.rootDir, url.url);
+
         console.log(req.method + ": " + file);
 
         if (url.url === "weaki-search") {
@@ -50,17 +71,17 @@ function requestHandler(opts) {
             }
 
             var ext = path.extname(file).substr(1).toLowerCase();
-            if (ext === 'md' || ext === 'markdown') {
-                h.doc(file, res, ext);
+            if (opts.inlineExt.indexOf(ext) !== -1) {
+                h.doc(file, res, ext, generateBreadcrumbs(opts, file));
                 return;
             }
 
             if (stat.isDirectory()) {
-                h.dir(file, res);
+                h.dir(file, res, generateBreadcrumbs(opts, file));
                 return;
             }
 
             h.file(file, stat, req, res);
         });
-    }
+    };
 }
