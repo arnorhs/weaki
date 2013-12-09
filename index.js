2ca5573282670e8861444945d97af43bc15c5e83
arnorhs <arnorhs@gmail.com>
2013-12-09 03:07:55 +0000

    move things more

--- index.js
+++ index.js
@@ -2,10 +2,19 @@ var server = require('./lib/server.js');
 var path = require('path');
 var respond = require('./lib/respond');
 var fs = require('fs');
+var handlers = require('./lib/response-handlers');
 
-var rootDir = process.cwd();
+module.exports = function(opts) {
+    // XXX Add xtend or something to do this
+    opts = opts || {};
+    opts.rootDir = opts.rootDir || process.cwd();
+    server(requestHandler(opts));
+};
 
-var handlers = require('./lib/response-handlers/')(rootDir);
+// if node index.js was called, start inline with no options
+if (!module.parent) {
+    module.exports();
+}
 
 function urler(url) {
     var parts = url.split('?');
@@ -16,39 +25,42 @@ function urler(url) {
     };
 }
 
-server(function(req, res) {
+function requestHandler(opts) {
+    var h = handlers(opts.rootDir);
 
-    var url = urler(req.url);
-    var file = path.resolve(rootDir, url.url);
-    console.log(req.method + ": " + file);
+    return function(req, res) {
+        var url = urler(req.url);
+        var file = path.resolve(opts.rootDir, url.url);
+        console.log(req.method + ": " + file);
 
-    if (url.url === "weaki-search") {
-        res.statusCode = 200;
-        res.end('search');
-        return;
-    }
+        if (url.url === "weaki-search") {
+            res.statusCode = 200;
+            res.end('search');
+            return;
+        }
 
-    fs.stat(file, function(err, stat) {
-        if (err) {
-            if (err && err.code === 'ENOENT') {
-                return respond(404, res);
-            } else {
-                console.log(err.stack);
-                return respond(500, res);
+        fs.stat(file, function(err, stat) {
+            if (err) {
+                if (err && err.code === 'ENOENT') {
+                    return respond(404, res);
+                } else {
+                    console.log(err.stack);
+                    return respond(500, res);
+                }
             }
-        }
 
-        var ext = path.extname(file).substr(1).toLowerCase();
-        if (ext === 'md' || ext === 'markdown') {
-            handlers.markdown(file, res);
-            return;
-        }
+            var ext = path.extname(file).substr(1).toLowerCase();
+            if (ext === 'md' || ext === 'markdown') {
+                h.doc(file, res, ext);
+                return;
+            }
 
-        if (stat.isDirectory()) {
-            handlers.dir(file, res);
-            return;
-        }
+            if (stat.isDirectory()) {
+                h.dir(file, res);
+                return;
+            }
 
-        handlers.file(file, stat, req, res);
-    });
-});
+            h.file(file, stat, req, res);
+        });
+    }
+}
