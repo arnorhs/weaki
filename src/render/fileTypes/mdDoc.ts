20bf7f0712da19a349aa54590d086f1f6f639deb
arnorhs <arnorhs@gmail.com>
2022-01-16 16:49:42 +0000

    newer marked

--- src/render/fileTypes/mdDoc.ts
+++ src/render/fileTypes/mdDoc.ts
@@ -1,9 +1,9 @@
-import marked from 'marked'
+import { parse } from 'marked'
 import { ForRendering } from '../types'
 
 export const renderMdDoc = (data: string): ForRendering => ({
   name: 'doc',
   locals: {
-    content: marked(data),
+    content: parse(data),
   },
 })
